f559cc637dd8b747294263c4c89d40a8979f345c
janos870 <janos.balogh870@gmail.com>
2023-05-09 21:12:53 +0200

    added note app

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from '@mui/material/Fab';
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    event.preventDefault();
+    props.onAdd(note);
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        <input
+          onChange={handleChange}
+          name="title"
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <Fab onClick={submitNote}> <AddIcon /> </Fab>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
